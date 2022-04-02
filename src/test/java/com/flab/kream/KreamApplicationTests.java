3613ae9508414409b0d4ceeb528084e327159e1b
newhyodong <hyodg90@gmail.com>
2022-04-02 17:28:18 +0900

    Revert "- test"
    This reverts commit 76bd05d80ef81318e75e2f9f4bef15ea4ac76254.

--- src/test/java/com/flab/kream/KreamApplicationTests.java
+++ src/test/java/com/flab/kream/KreamApplicationTests.java
@@ -1,36 +1,13 @@
 package com.flab.kream;
 
-import com.flab.model.Member;
-import com.flab.service.MemberService;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest
 @SpringBootTest
 class KreamApplicationTests {
-
-	@Autowired(required=false)
-	MemberService memberService;
-
-	@Autowired
-	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
 	}
 
-	@Test
-	@DisplayName("멤버조회")
-	void test(){
-		Member member = memberService.getMember("testid");
-		System.out.println("member = " + member.toString());
-
-	}
 }
