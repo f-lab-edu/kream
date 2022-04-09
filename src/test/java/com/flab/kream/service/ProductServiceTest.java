48cda9610f338dc8e3e9f572dd5a7713b49de801
seosanggi <kongakilla@gmail.com>
2022-04-09 20:36:50 +0900

    product service test작성중

---
+++ src/test/java/com/flab/kream/service/ProductServiceTest.java
@@ -0,0 +1,39 @@
+package com.flab.kream.service;
+
+import com.flab.kream.product.dao.ProductDAO;
+import com.flab.kream.product.dto.ProductDTO;
+import com.flab.kream.product.service.ProductService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Spy;
+
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+class ProductServiceTest {
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Spy
+    private ProductDAO productDAO;
+
+    @Test
+    void registrationProductTest() {
+
+        //given
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setProductId(000);
+        productDTO.setPrice("2000000");
+        productDTO.setBrandId(000);
+        productDTO.setImageUrl("test");
+
+        //when
+        productService.registrationProduct(productDTO);
+
+        //then
+
+    }
+
+}
