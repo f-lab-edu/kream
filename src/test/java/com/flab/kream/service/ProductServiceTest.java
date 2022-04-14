a7e4314177ca6e9fbead6d35d41d4b8fb5581bde
seosanggi <kongakilla@gmail.com>
2022-04-14 22:05:24 +0900

    product service test에서 상품생성 메소드 작성

--- src/test/java/com/flab/kream/service/ProductServiceTest.java
+++ src/test/java/com/flab/kream/service/ProductServiceTest.java
@@ -13,6 +13,9 @@ import static org.mockito.Mockito.verify;
 
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ExtendWith(SpringExtension.class)
 class ProductServiceTest {
 
@@ -21,6 +24,17 @@ class ProductServiceTest {
 
     @Spy
     private ProductDAO productDAO;
+
+
+    public ProductDTO createProduct(int productId) {
+        ProductDTO productDTO = new ProductDTO();
+        productDTO.setProductId(productId);
+        productDTO.setPrice("1000000");
+        productDTO.setBrandId(1);
+        productDTO.setImageUrl("test");
+
+        return productDTO;
+    }
 
     @Test
     void registrationProductTest() {
@@ -38,6 +52,17 @@ class ProductServiceTest {
         //then
         verify(productDAO).registrationProduct(any(ProductDTO.class));
 
+    }
+
+    @Test
+    void findProductListTest() {
+        //given
+        List<ProductDTO> prductList = new ArrayList<>();
+
+
+        //when
+
+        //then
     }
 
 }
