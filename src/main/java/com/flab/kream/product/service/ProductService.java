3bf91a7f94efd7784dd955cf22ceb8f32dca6b2a
seosanggi <kongakilla@gmail.com>
2022-04-14 21:57:43 +0900

    상품조회 service작성

--- src/main/java/com/flab/kream/product/service/ProductService.java
+++ src/main/java/com/flab/kream/product/service/ProductService.java
@@ -1,9 +1,14 @@
 package com.flab.kream.product.service;
 
+import com.flab.kream.common.model.Pagination;
 import com.flab.kream.product.dto.ProductDTO;
+
+import java.util.List;
 
 public interface ProductService {
 
-    public void registrationProduct(ProductDTO productDto);
+    void registrationProduct(ProductDTO productDto);
+
+    List<ProductDTO> findProductList(Pagination pagination);
 
 }
--- src/main/java/com/flab/kream/product/service/ProductServiceImpl.java
+++ src/main/java/com/flab/kream/product/service/ProductServiceImpl.java
@@ -1,8 +1,12 @@
 package com.flab.kream.product.service;
 
+import com.flab.kream.common.model.Pagination;
 import com.flab.kream.product.dao.ProductDAO;
 import com.flab.kream.product.dto.ProductDTO;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -13,9 +17,16 @@ public class ProductServiceImpl implements ProductService {
         this.productDAO = productDAO;
     }
 
+    @Transactional
     @Override
     public void registrationProduct(ProductDTO productDto) {
         productDAO.registrationProduct(productDto);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<ProductDTO> findProductList(Pagination pagination) {
+        return productDAO.getProductList(pagination);
     }
 
 }
