5acb37d5b7492eb79613c41bfdd904510630205e
seosanggi <kongakilla@gmail.com>
2022-04-14 21:51:16 +0900

    상품조회 SQL작성

--- src/main/java/com/flab/kream/product/dao/ProductDAO.java
+++ src/main/java/com/flab/kream/product/dao/ProductDAO.java
@@ -1,13 +1,18 @@
 package com.flab.kream.product.dao;
 
+import com.flab.kream.common.model.Pagination;
 import com.flab.kream.product.dto.ProductDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
 public interface ProductDAO {
 
     void registrationProduct( ProductDTO productDTO );
+
+    List<ProductDTO> getProductList( Pagination pagination );
 
 }
