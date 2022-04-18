edc338ba1dd3e8ec0f2a50d27d5c802353bf99fd
seosanggi <kongakilla@gmail.com>
2022-04-18 23:57:17 +0900

    service interface - 구현체class로 이루어져 있던걸 변경

--- src/main/java/com/flab/kream/product/service/ProductService.java
+++ src/main/java/com/flab/kream/product/service/ProductService.java
@@ -1,9 +1,18 @@
 package com.flab.kream.product.service;
 
+import com.flab.kream.product.dao.ProductDAO;
 import com.flab.kream.product.dto.ProductDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface ProductService {
+@Service
+@RequiredArgsConstructor
+public class ProductService {
 
-    public void registrationProduct(ProductDTO productDto);
+    private final ProductDAO productDAO;
+
+    public void registrationProduct(ProductDTO productDto) {
+        productDAO.registrationProduct(productDto);
+    }
 
 }
