62c779378934de9dbc4fee367f4846e15a63de77
seosanggi <kongakilla@gmail.com>
2022-04-29 01:04:08 +0900

    상품등록시 유효성테스트 추가

--- src/test/java/com/flab/kream/service/ProductServiceTest.java
+++ src/test/java/com/flab/kream/service/ProductServiceTest.java
@@ -1,21 +1,33 @@
 package com.flab.kream.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.flab.kream.product.dao.ProductMapper;
 import com.flab.kream.product.dto.ProductRequestDTO;
 import com.flab.kream.product.service.ProductService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.verify;
-
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 class ProductServiceTest {
+
+    private static ValidatorFactory factory;
+    private static Validator validator;
 
     @InjectMocks
     private ProductService productService;
@@ -23,8 +35,15 @@ class ProductServiceTest {
     @Spy
     private ProductMapper productMapper;
 
+    @BeforeAll
+    public static void init(){
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+
     @Test
-    @DisplayName("상품등록테스트")
+    @DisplayName("상품등록테스트 - 성공")
     void registProductTest() {
 
         //given
@@ -53,9 +72,12 @@ class ProductServiceTest {
         productDTO.setImageUrl(null);
 
         //when
-
+        Set<ConstraintViolation<ProductRequestDTO>> validate = validator.validate(productDTO);
 
         //then
+        validate.forEach(error -> {
+            assertThat(error.getMessage()).isEqualTo("정확한 가격을 입력해주세요");
+        });
 
     }
 
