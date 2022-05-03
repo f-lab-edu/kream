310c30b58ed7d426b805cea38fbc46bdbe8cee28
seosanggi <kongakilla@gmail.com>
2022-05-03 23:18:30 +0900

    product Controller 의존성주입 lombok @RequiredArgsConstructor적용

--- src/main/java/com/flab/kream/product/ProductController.java
+++ src/main/java/com/flab/kream/product/ProductController.java
@@ -2,6 +2,7 @@ package com.flab.kream.product;
 
 import com.flab.kream.product.dto.ProductRequestDTO;
 import com.flab.kream.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,21 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
 
     @PostMapping
     public ResponseEntity<HttpStatus> registration(@Valid @RequestBody ProductRequestDTO requestDTO) {
-
         productService.registProduct(requestDTO);
-
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
