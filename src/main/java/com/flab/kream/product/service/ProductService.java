004214364692deac6650d30d92a1e3398077a2c5
seosanggi <kongakilla@gmail.com>
2022-05-15 01:28:04 +0900

    select api

--- src/main/java/com/flab/kream/product/service/ProductService.java
+++ src/main/java/com/flab/kream/product/service/ProductService.java
@@ -1,9 +1,13 @@
 package com.flab.kream.product.service;
 
+import com.flab.kream.common.model.Pagination;
 import com.flab.kream.product.dao.ProductMapper;
 import com.flab.kream.product.dto.ProductRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +15,14 @@ public class ProductService {
 
     private final ProductMapper productMapper;
 
+    @Transactional(rollbackFor = Exception.class)
     public void registProduct(ProductRequestDTO requestDTO) {
         productMapper.insertProduct(requestDTO);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProductRequestDTO> findProductList(Pagination pagination) {
+        return productMapper.getProductList(pagination);
     }
 
 }
